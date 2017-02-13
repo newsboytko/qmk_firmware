b045e042bd26d302fc98d19b6244c27da435bb16
Gabriel Young <gabeplaysdrums@live.com>
2017-02-12 19:51:50 -0800

    add default layer toggle for mac vs windows

--- keyboards/satan/keymaps/newsboytko/keymap.c
+++ keyboards/satan/keymaps/newsboytko/keymap.c
@@ -3,20 +3,28 @@
 // Readability
 #define _______ KC_TRNS
 
-// Used for SHIFT_ESC
-#define MODS_SHIFT_MASK  (MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT))
-
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
-#define _BL 0
-#define _FL 1
-#define _RL 2
+enum layer_id
+{
+  _BL_WINDOWS,
+  _BL_MAC,
+  _BL_MAX,
+  _FL = _BL_MAX,
+  _RL,
+};
+
+enum function_id {
+    SHIFT_ESC,
+    NEXT_DEFAULT_LAYER,
+};
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  /* Keymap _BL: (Base Layer) Default Layer
+  /* Keymap _BL_WINDOWS: (Base Layer) Default Layer for Windows
    * ,------------------------------------------------------------------------.
    * |Esc~|   1|   2|   3|   4|   5|   6|   7|   8|   9|   0|   -|   =|Backspc|
    * |------------------------------------------------------------------------|
@@ -35,12 +43,39 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *                |   FN   |         | Shift  |
    *                `--------'         `--------'
    */
-[_BL] = KEYMAP_ANSI_FOOTSWITCHES(
-  F(0),    KC_1,    KC_2,    KC_3,   KC_4,    KC_5,    KC_6,    KC_7,     KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSPC, \
+[_BL_WINDOWS] = KEYMAP_ANSI_FOOTSWITCHES(
+  F(SHIFT_ESC),    KC_1,    KC_2,    KC_3,   KC_4,    KC_5,    KC_6,    KC_7,     KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSPC, \
   KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,    KC_Y,    KC_U,     KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, \
   MO(_FL), KC_A,    KC_S,    KC_D,   KC_F,    KC_G,    KC_H,    KC_J,     KC_K,    KC_L,   KC_SCLN, KC_QUOT,          KC_ENT,  \
   KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,    KC_N,    KC_M,     KC_COMM, KC_DOT, KC_SLSH,                   KC_RSFT, \
   KC_LCTL, KC_LGUI, KC_LALT,                           KC_SPC,                             KC_RALT, MO(_RL), MO(_FL), KC_RCTL, \
+  MO(_FL), KC_LSFT),
+
+  /* Keymap _BL: (Base Layer) Default Layer for Mac
+   * ,------------------------------------------------------------------------.
+   * |Esc~|   1|   2|   3|   4|   5|   6|   7|   8|   9|   0|   -|   =|Backspc|
+   * |------------------------------------------------------------------------|
+   * |Tab   |   Q|   W|   E|   R|   T|   Y|   U|   I|   O|   P|   [|   ]|  \  |
+   * |------------------------------------------------------------------------|
+   * | FN    |   A|   S|   D|   F|   G|   H|   J|   K|   L|   ;|   '|  Return |
+   * |------------------------------------------------------------------------|
+   * |Shift    |   Z|   X|   C|   V|   B|   N|   M|   ,|   .|   /|      Shift |
+   * |------------------------------------------------------------------------|
+   * |Ctrl | Alt | Gui |            Space             | Gui | RL  | FN  | Ctrl|
+   * `------------------------------------------------------------------------'
+   *
+   * Foot switches:
+   *                ,--------.         ,--------.
+   *                |        |         |        |
+   *                |   FN   |         | Shift  |
+   *                `--------'         `--------'
+   */
+[_BL_MAC] = KEYMAP_ANSI_FOOTSWITCHES(
+  F(SHIFT_ESC),    KC_1,    KC_2,    KC_3,   KC_4,    KC_5,    KC_6,    KC_7,     KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSPC, \
+  KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,    KC_Y,    KC_U,     KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, \
+  MO(_FL), KC_A,    KC_S,    KC_D,   KC_F,    KC_G,    KC_H,    KC_J,     KC_K,    KC_L,   KC_SCLN, KC_QUOT,          KC_ENT,  \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,    KC_N,    KC_M,     KC_COMM, KC_DOT, KC_SLSH,                   KC_RSFT, \
+  KC_LCTL, KC_LALT, KC_LGUI,                           KC_SPC,                             KC_RGUI, MO(_RL), MO(_FL), KC_RCTL, \
   MO(_FL), KC_LSFT),
 
 /* Keymap _FL: Function Layer
@@ -95,48 +130,59 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, BL_DEC,  BL_INC,  BL_TOGG, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, \
   _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______,                   _______, \
-  _______, _______, _______,                            _______,                            _______, _______, _______, _______, \
+  _______, F(NEXT_DEFAULT_LAYER), _______,                _______,                          _______, _______, _______, _______, \
   _______, _______),
   #else
   _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET,   \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, BL_DEC,  BL_INC,  BL_TOGG, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______, \
-  _______, _______, _______,                            _______,                            _______, _______, _______, _______, \
+  _______, F(NEXT_DEFAULT_LAYER), _______,                _______,                          _______, _______, _______, _______, \
   _______, _______),
   #endif
 };
 
-enum function_id {
-    SHIFT_ESC,
+const uint16_t PROGMEM fn_actions[] = {
+  [SHIFT_ESC] = ACTION_FUNCTION(SHIFT_ESC),
+  [NEXT_DEFAULT_LAYER] = ACTION_FUNCTION(NEXT_DEFAULT_LAYER),
 };
 
-const uint16_t PROGMEM fn_actions[] = {
-  [0]  = ACTION_FUNCTION(SHIFT_ESC),
-};
+// Used for SHIFT_ESC
+#define MODS_SHIFT_MASK  (MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT))
+
+// used for NEXT_DEFAULT_LAYER
+static uint16_t current_default_layer = 0;
 
 void action_function(keyrecord_t *record, uint8_t id, uint8_t opt) {
-  uint8_t shift_pressed;
-  shift_pressed = get_mods() & MODS_SHIFT_MASK;
-  switch (id) {
-    case SHIFT_ESC:
-      if (record->event.pressed) {
-        if (shift_pressed) {
-          add_key(KC_GRV);
-          send_keyboard_report();
-        } else {
-          add_key(KC_ESC);
-          send_keyboard_report();
+    uint8_t shift_pressed;
+    shift_pressed = get_mods() & MODS_SHIFT_MASK;
+    switch (id) {
+        case SHIFT_ESC:
+        {
+            if (record->event.pressed) {
+                if (shift_pressed) {
+                    add_key(KC_GRV);
+                    send_keyboard_report();
+                } else {
+                    add_key(KC_ESC);
+                    send_keyboard_report();
+                }
+            } else {
+                if (shift_pressed) {
+                    del_key(KC_GRV);
+                    send_keyboard_report();
+                } else {
+                    del_key(KC_ESC);
+                    send_keyboard_report();
+                }
+            }
+            break;
         }
-      } else {
-        if (shift_pressed) {
-          del_key(KC_GRV);
-          send_keyboard_report();
-        } else {
-          del_key(KC_ESC);
-          send_keyboard_report();
+        case NEXT_DEFAULT_LAYER:
+        {
+        	current_default_layer = (current_default_layer + 1) % _BL_MAX;
+        	default_layer_set(1UL << current_default_layer);
+            break;
         }
-      }
-      break;
-  }
+    }
 }
