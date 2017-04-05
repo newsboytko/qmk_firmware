5bd5db7c322abc5a09f48927ea9eb7daf6550657
Gabriel Young <gabeplaysdrums@live.com>
2017-04-05 08:34:03 -0700

    add kvm switcher to windows gui layer

--- keyboards/frosty_flake/keymaps/newsboytko/keymap.c
+++ keyboards/frosty_flake/keymaps/newsboytko/keymap.c
@@ -14,3 +14,13 @@ void action_function(keyrecord_t* record, uint8_t id, uint8_t opt) {
         return;
     //TODO: add keyboard-specific actions here
 }
+
+const macro_t *action_get_macro(keyrecord_t* record, uint8_t id, uint8_t opt) {
+    const macro_t* macro = common_action_get_macro(record, id, opt);
+    if (macro)
+        return macro;
+
+    //TODO: add keyboard-specific macros here
+
+    return MACRO_NONE;
+}
