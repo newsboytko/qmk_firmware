d1a0528debf2db87db46d6f9de157fc5b0d7a2ac
Gabriel Young <gabeplaysdrums@live.com>
2018-01-30 21:36:20 -0800

    Add OS-independent commands

--- keyboards/ergodox_infinity/keymaps/gabeplaysdrums/keymap.c
+++ keyboards/ergodox_infinity/keymaps/gabeplaysdrums/keymap.c
@@ -17,9 +17,20 @@ enum custom_keycodes {
     MAKE = SAFE_RANGE,
     MAKE_RIGHT,
     CPP_DEREF,
-    OS_MOD_WORD,
-    OS_MOD_OPT,
     OS_TOGGLE,
+    OS_MOD_WORD,
+    OS_SAVE,
+    OS_QUIT,
+    OS_UNDO,
+    OS_CUT,
+    OS_COPY,
+    OS_PASTE,
+    OS_SELECTALL,
+    OS_LAUNCH,
+    OS_RUN,
+    OS_SWITCH_TASK,
+    OS_NEXT_TASK,
+    OS_PREV_TASK,
     MY_SAFE_RANGE,
 };
 
@@ -47,6 +58,14 @@ void eeconfig_update_my_config(uint32_t val) {
   eeprom_update_dword(EECONFIG_GABEPLAYSDRUMS, val);
 }
 
+typedef struct {
+    bool task_switcher_active;
+} my_state_t;
+
+static my_state_t my_state = {
+    .task_switcher_active = false,
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Base Layer
@@ -54,10 +73,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * | Esc    |   1  |   2  |   3  |   4  |   5  |      |           |      |   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  |      |           |      |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  | COPY |           |      |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | SYMB   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * | SYMB   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |J/Tsk+|K/Tsk-|   L  |   ;  |   '    |
+ * |--------+------+------+------+------+------| PASTE|           |      |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |      |      |      |      |      |                                       | GUI  |      |      | SYMB | FUNC |
@@ -67,29 +86,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 ,------|------|------|       |------+------+------.
  *                                 |      |      |      |       |      |      |      |
  *                                 | Bksp | FUNC |------|       |------|Enter |Space |
- *                                 |      |      |      |       |      |      |      |
+ *                                 |      |      | TASK |       |LAUNCH|      |      |
  *                                 `--------------------'       `--------------------'
  */
 [BASE] = KEYMAP(
         // left hand
         KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, XXXXX,
-        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, XXXXX,
+        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, OS_COPY,
         MO(SYMB), KC_A, KC_S, KC_D, KC_F, KC_G,
-        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, XXXXX,
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, OS_PASTE,
         KC_LCTL, KC_LGUI, KC_LALT, XXXXX, XXXXX,
                                                         XXXXX, XXXXX,
                                                         XXXXX,
-                                                        KC_BSPC, TT(FUNC), XXXXX,
+                                                        KC_BSPC, TT(FUNC), OS_SWITCH_TASK,
 
         // right hand
                                 XXXXX, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS,
                                 XXXXX, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS,
-                                KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
+                                KC_H, OS_NEXT_TASK, OS_PREV_TASK, KC_L, KC_SCLN, KC_QUOT,
                                 XXXXX, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
                                 KC_RGUI, XXXXX, XXXXX, TT(SYMB), TT(FUNC),
         XXXXX, XXXXX,
         XXXXX,
-        XXXXX, KC_ENT, KC_SPC
+        OS_LAUNCH, KC_ENT, KC_SPC
     ),
 
 /* Symbol Layer
@@ -109,7 +128,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                        |      |      |       |      |      |
  *                                 ,------|------|------|       |------+------+------.
  *                                 |      |      |      |       |      |      |      |
- *                                 |      |      |------|       |------|      |      |
+ *                                 |      |      |------|       |------|      |   _  |
  *                                 |      |      |      |       |      |      |      |
  *                                 `--------------------'       `--------------------'
  */
@@ -132,7 +151,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                 _____, _____, _____, _____, _____,
         _____, _____,
         _____,
-        _____, _____, _____
+        _____, _____, KC_UNDS
     ),
 
 /* Function Layer
@@ -140,11 +159,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |        |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  | F10  |  F11   |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |           |      | PgUp | Home |  Up  | End  |      |  F12   |
+ * |        | QUIT |      |      | RUN  |      |      |           |      | PgUp | Home |  Up  | End  |      |  F12   |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      | Ctrl |Shift | Alt  |      |------|           |------| PgDn | Left | Down | Right|      |        |
+ * |        |SELALL| SAVE |Shift | WORD |      |------|           |------| PgDn | Left | Down | Right|      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+ * |        | UNDO | CUT  | COPY |PASTE |      |      |           |      |      |      |      |      |      |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |      |      |      |      |      |                                       |      |      |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
@@ -159,9 +178,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [FUNC] = KEYMAP(
         // left hand
         _____, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, _____,
-        _____, _____, _____, _____, _____, _____, _____,
-        _____, _____, OS_MOD_OPT, KC_LSFT, OS_MOD_WORD, _____,
-        _____, _____, _____, _____, _____, _____, _____,
+        _____, OS_QUIT, _____, _____, OS_RUN, _____, _____,
+        _____, OS_SELECTALL, OS_SAVE, KC_LSFT, OS_MOD_WORD, _____,
+        _____, OS_UNDO, OS_CUT, OS_COPY, OS_PASTE, _____, _____,
         _____, _____, _____, _____, _____,
                                                         MAKE, RESET,
                                                         _____,
@@ -244,6 +263,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 SEND_STRING("->");
             }
             return false;
+        case OS_TOGGLE:
+            if (record->event.pressed) {
+                ++my_config.os;
+                if (my_config.os >= OS_MAX) my_config.os = 0;
+                eeconfig_update_my_config(my_config.raw);
+            }
+            return false;
         case OS_MOD_WORD:
         {
             uint8_t code;
@@ -264,33 +290,131 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
             return false;
         }
-        case OS_MOD_OPT:
+        case OS_QUIT:
         {
-            uint8_t code;
-
-            if (my_config.os == OS_WINDOWS) {
-                code = KC_LALT;
-            }
-            else { // my_config.os == OS_MAC
-                code = KC_LCTL;
-            }
-
             if (record->event.pressed) {
-                register_code(code);
-            }
-            else {
-                unregister_code(code);
+                if (my_config.os == OS_WINDOWS) {
+                    SEND_STRING(SS_DOWN(X_LALT) SS_TAP(X_F4) SS_UP(X_LALT));
+                }
+                else { // my_config.os == OS_MAC
+                    SEND_STRING(SS_LGUI("q"));
+                }
             }
 
             return false;
         }
-        case OS_TOGGLE:
+        case OS_LAUNCH:
+        {
             if (record->event.pressed) {
-                ++my_config.os;
-                if (my_config.os >= OS_MAX) my_config.os = 0;
-                eeconfig_update_my_config(my_config.raw);
+                if (my_config.os == OS_WINDOWS) {
+                    SEND_STRING(SS_TAP(X_LGUI));
+                }
+                else { // my_config.os == OS_MAC
+                    SEND_STRING(SS_LCTRL(" "));
+                }
             }
+
             return false;
+        }
+        case OS_RUN:
+        {
+            if (record->event.pressed) {
+                if (my_config.os == OS_WINDOWS) {
+                    SEND_STRING(SS_LGUI("r"));
+                }
+                else { // my_config.os == OS_MAC
+                    SEND_STRING(SS_LCTRL(" ") "terminal" SS_TAP(X_ENTER));
+                }
+            }
+
+            return false;
+        }
+        case OS_SWITCH_TASK:
+        {
+            if (record->event.pressed) {
+                if (my_config.os == OS_WINDOWS) {
+                    SEND_STRING(SS_DOWN(X_LALT) SS_TAP(X_TAB));
+                }
+                else { // my_config.os == OS_MAC
+                    SEND_STRING(SS_DOWN(X_LGUI) SS_TAP(X_TAB));
+                }
+                my_state.task_switcher_active = true;
+            }
+            else
+            {
+                if (my_config.os == OS_WINDOWS) {
+                    SEND_STRING(SS_UP(X_LALT));
+                }
+                else { // my_config.os == OS_MAC
+                    SEND_STRING(SS_UP(X_LGUI));
+                }
+                my_state.task_switcher_active = false;
+            }
+
+            return false;
+        }
+        case OS_NEXT_TASK:
+        {
+            if (my_state.task_switcher_active) {
+                if (record->event.pressed) {
+                    SEND_STRING(SS_TAP(X_TAB));
+                }
+            }
+            else {
+                if (record->event.pressed) {
+                    register_code(KC_J);
+                }
+                else
+                {
+                    unregister_code(KC_J);
+                }
+            }
+
+            return false;
+        }
+        case OS_PREV_TASK:
+        {
+            if (my_state.task_switcher_active) {
+                if (record->event.pressed) {
+                    SEND_STRING(SS_DOWN(X_LSHIFT) SS_TAP(X_TAB) SS_UP(X_LSHIFT));
+                }
+            }
+            else {
+                if (record->event.pressed) {
+                    register_code(KC_K);
+                }
+                else
+                {
+                    unregister_code(KC_K);
+                }
+            }
+
+            return false;
+        }
+
+#define DEFINE_BASIC_OS_COMMAND(kc, shortcut) \
+        case kc: \
+        { \
+            if (record->event.pressed) { \
+                if (my_config.os == OS_WINDOWS) { \
+                    SEND_STRING(SS_LCTRL(shortcut)); \
+                } \
+                else { /* my_config.os == OS_MAC */ \
+                    SEND_STRING(SS_LGUI(shortcut)); \
+                } \
+            } \
+            return false; \
+        }
+
+        DEFINE_BASIC_OS_COMMAND(OS_SELECTALL, "a")
+        DEFINE_BASIC_OS_COMMAND(OS_SAVE, "s")
+        DEFINE_BASIC_OS_COMMAND(OS_UNDO, "z")
+        DEFINE_BASIC_OS_COMMAND(OS_CUT, "x")
+        DEFINE_BASIC_OS_COMMAND(OS_COPY, "c")
+        DEFINE_BASIC_OS_COMMAND(OS_PASTE, "v")
+
+#undef DEFINE_BASIC_OS_COMMAND
+
         default:
           return true;
     }
